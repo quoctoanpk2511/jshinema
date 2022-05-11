e09cdac566e6dbe48e1c5877d82b594666c98dd0
quoctoanpk2511 <quoctoanpk2511@gmail.com>
2022-05-11 19:44:05 +0700

    initial commit

---
+++ src/main/java/com/jshinema/JshinemaApplication.java
@@ -0,0 +1,13 @@
+package com.jshinema;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JshinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JshinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jshinema/JshinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jshinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JshinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
